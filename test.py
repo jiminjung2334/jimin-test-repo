9240273611a5dea222749f5acc1b3e539aca908d
jiminjung2334 <101380950+jiminjung2334@users.noreply.github.com>
2022-05-03 00:01:27 +0900

    Update test.py
    나의 개인정보를  print 하였다.

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 #Jimin Jung, 202103173, computer engineering
-#정지민, 202103173, 컴퓨터공학부
 
 print("test")
 print("hello hufs")
@@ -7,3 +6,6 @@ print("Python")
 print("good bye")
 
 print("오픈소스 실습용")
+print("이름 : 정지민")
+print("학번 : 202103173")
+print("학과 : 컴퓨터공학부")
